8dfc4986fa506f49124dc79fdb2a4faaac2f870e
LERodolfo0705 <rodolfo20452@cbtis75.edu.mx>
2022-09-30 09:26:49 -0500

    30 sep

---
+++ parcial_2/hoy.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style2.css" type="text/css">
+    <title>Hola Mundo</title>
+</head>
+
+<body>
+    <?php
+
+    echo date("28-09-2022");
+
+    ?>
+</body>
+
+</html>
