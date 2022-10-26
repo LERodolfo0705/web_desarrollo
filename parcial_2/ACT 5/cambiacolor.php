85beaf1e59a3d81f94cbe79e79e5800cd6fb831d
LERodolfo0705 <114744827+LERodolfo0705@users.noreply.github.com>
2022-10-26 11:05:59 -0500

    05/10/22

---
+++ parcial_2/ACT 5/cambiacolor.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actividad 4</title>
+
+</head>
+<!--Rodolfo Loredo Estrada 
+          5 PM-->
+<body>
+    <h1 id="samu"> ¡¡Soy una simple página!!  Rodolfo Loredo Estrada 5 PM</h1>
+    <button id="btnrojo" style="background-color:red;" onmouseover="rojo()">Red</button>
+    <button id="btnazul" style="background-color:blue;" onmouseover="azul()">Blue</button>
+    <button id="btnverde" style="background-color:green;" onmouseover="verde()">Green</button>
+
+    
+    <script>
+        function rojo() {
+            //alert("Outchh soy rojo");
+            document.getElementById("samu").style.backgroundColor ="red";
+        }
+
+        function azul() {
+            //alert("Outchh soy azul");
+            document.getElementById("samu").style.backgroundColor ="blue";
+        }
+
+        function verde() {
+            //alert("Outchh soy verde");
+            document.getElementById("samu").style.backgroundColor ="green";
+        }
+    </script>
+</body>
+
+</html>
